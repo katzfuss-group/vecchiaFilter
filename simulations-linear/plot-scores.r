b7fdad834f7df09a78f90a4ebb1f83a3f9ba36dd
marcinjurek <marcinjurek1988@gmail.com>
2020-01-23 10:01:43 -0600

    further reorganize simulations folder

---
+++ simulations-linear/plot-scores.r
@@ -0,0 +1,7 @@
+setwd("~/HVLF")
+source("plotting-functions.r")
+resultsDir="simulations-linear"
+data.models = c("gauss")
+plotScores(resultsDir, data.models)
+plotSims(resultsDir, "gauss", 1)
+#plotSims(resultsDir, "gauss", 2)
