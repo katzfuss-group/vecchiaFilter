7fef7cb13cbf859e52807288a6e0341180e0596f
marcinjurek <marcinjurek1988@gmail.com>
2020-02-19 10:46:25 -0600

    organize files

--- simulations-spatial/plot-scores.r
+++
@@ -1,8 +0,0 @@
-setwd("~/HVLF")
-source("plotting-functions.r")
-resultsDir="simulations-spatial"
-data.models = c("gauss")
-plotScores(resultsDir, data.models)
-#plotSims(resultsDir, "gauss", 1)
-#plotSims(resultsDir, "gauss", 2)
-rm(list=ls())
