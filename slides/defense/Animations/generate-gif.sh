86ade8433d19fc5a2d95434b02749312c6c662eb
marcinjurek <marcinjurek1988@gmail.com>
2020-06-26 16:40:07 -0500

    remove slides

--- slides/defense/Animations/generate-gif.sh
+++
@@ -1,7 +0,0 @@
-python /home/marcin/AgProject/plots-code/generateAgPlots.py
-cp /home/marcin/AgProject/plots/truth_* ~/MRF/slides/JSM-Summer18/Animations/
-cp /home/marcin/AgProject/plots/obs_* ~/MRF/slides/JSM-Summer18/Animations/
-
-rm truth.gif obs.gif
-convert -delay 20 -loop 0 truth_* truth.gif
-convert -delay 20 -loop 0 obs_* obs.gif
