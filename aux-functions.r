21eee74e0c2767c6fe95e77dad1fdf625512e165
Marcin Jurek <marcinjurek1988@gmail.com>
2021-02-23 10:49:53 -0600

    fix evolution function

--- aux-functions.r
+++ aux-functions.r
@@ -48,24 +48,48 @@ getConfInt = function(preds, alpha){
 evolAdvDiff = function(state, adv=0, diff=0){
   # we assume that there is the same number of grid points
   # along each dimension
-
     if(is.null(dim(state))) {
         N = length(state)
     } else {
         N = dim(state)[1]
     }
+
     Ny = Nx = sqrt(N)
   
     dx = dy = 1/Nx
-    d = diff/(dx**2)
+    D = diff/(dx**2)
+
+
+    c1 = 1 + 2*(D + D) - adv*(1/dx + 1/dy)
+    c2 = - D + adv*(1/dy)
+    c3 = - D
+
+    ## Let's consider the following layout on the grid
+    ## . . . . .
+    ## . . c . .
+    ## . a x b .
+    ## . . d . .
+    ## . . . . .
+    ## where the values "x" are given by d3, "d" - by d1,
+    ## "a" - by d2, "b" - by d4 and "c" by d5.
+    d = rep(c2, N-Nx)
+    a = rep(c2, N-1)
+    x = rep(c1, N)
+    b = rep(c3, N-1)
+    c = rep(c3,N-Nx)
+
+    # Now notice that for k = 1:n, in each kn-th row there should
+    # be no b and for every kn+1-st row there should be no a
+    inds.b = Nx*(1:(Ny-1))
+    inds.a = Nx*(1:(Ny-1))
+    b[inds.b] = 0
+    a[inds.a] = 0
     
-    c1 = 1 + 2*(d + d) - adv*(1/dx + 1/dy)
-    c2 = - d + adv*(1/dy)
-    c3 = - d
-  
-    diags = list(rep(c2, N-Nx), rep(c2, N-1), rep(c1, N), rep(c3, N-1), rep(c3,N-Nx) )
+    diags = list(d, a, x, b, c)
     E = Matrix::bandSparse(N, k=c(-Nx, -1, 0, 1, Nx), diag=diags)
-  
+
+    #browser()
+    
     if (class(state) == 'matrix' || methods::is(state, 'sparseMatrix')){
         return( E %*% state )
     } else {
@@ -123,36 +147,36 @@ simulate.y = function(x, frac.obs, lik.params){
 
 ## simulate x
 simulate.xy = function(x0, E, Q, frac.obs, lik.params, Tmax, seed=NULL, sig2=1, smooth = 0.5, range = 1, locs = NULL){
-  
-  if (!is.null(seed)) set.seed(seed)
-  n = nrow(x0);
-  x = list(); y = list()
-  x[[1]] = x0
-  y[[1]] = simulate.y(x0, frac.obs, lik.params)
-  
-  if (Tmax > 1) { 
-    
-    if (!is.null(Q) && any(Q)) {
-      Qc = Matrix::chol(Q)
-    } 
-    
-    for (t in 2:Tmax) {
-      if (sig2 > 0 || (!is.null(Q) && sum(abs(Q))>0)) {
-        if (!is.null(Q)) {
-          w =  t(Qc) %*% matrix(rnorm(n), ncol = 1)
-        } else {
-          w = matrix(RandomFields::RFsimulate(model = RandomFields::RMmatern(nu = smooth, scale = range, var = sig2),
-                                            x = locs[,1], y = locs[,2], spConform = FALSE), ncol=1)
-        } 
-      } else {
-        w = matrix(rep(0, n), ncol=1)
-      }
-      
-      x[[t]] = E(x[[t - 1]]) + w
-      y[[t]] = simulate.y(x[[t]], frac.obs, lik.params)
-    } 
-  }
-  
-  return(list(x = x, y = y))
 
+    if (!is.null(seed)) set.seed(seed)
+    n = nrow(x0);
+    x = list(); y = list()
+    x[[1]] = x0
+    y[[1]] = simulate.y(x0, frac.obs, lik.params)
+    
+    
+    if (Tmax > 1) { 
+        if (!is.null(Q) && any(Q)) {
+            Qc = Matrix::chol(Q)
+        } 
+        
+        for (t in 2:Tmax) {
+            cat(sprintf("simulating at time t=%d\n", t))
+            if (sig2 > 0 || (!is.null(Q) && sum(abs(Q))>0)) {
+                if (!is.null(Q)) {
+                    w =  t(Qc) %*% matrix(rnorm(n), ncol = 1)
+              } else {
+                  w = matrix(RandomFields::RFsimulate(model = RandomFields::RMmatern(nu = smooth, scale = range, var = sig2),
+                                                      x = locs[,1], y = locs[,2], spConform = FALSE), ncol=1)
+              } 
+            } else {
+                w = matrix(rep(0, n), ncol=1)
+            }
+            x[[t]] = E(x[[t - 1]]) + w
+            y[[t]] = simulate.y(x[[t]], frac.obs, lik.params)
+        } 
+    }
+    
+    return(list(x = x, y = y))
+    
 }
