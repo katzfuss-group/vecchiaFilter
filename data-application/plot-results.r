1f5825d63787179b4c666047120be54ee764e7a2
Marcin Jurek <marcinjurek1988@gmail.com>
2021-04-08 11:15:14 -0500

    create a separate file for plotting results

---
+++ data-application/plot-results.r
@@ -0,0 +1,120 @@
+plotFields = function(XY, preds, locs) {
+
+    Tmax = length(XY$x)
+    N = nrow(locs)
+    if (round(sqrt(N)) != sqrt(N)) {
+        warning("The size of the grid is not a square of an integer. Plots might look weird")
+    }
+    
+    zlim_x = range(sapply(1:Tmax, function(t) range(XY$x[[t]])), XY$x0)
+    zlim_y = range(sapply(1:Tmax, function(t) range(XY$y[[t]])))
+    zlim_p = range(sapply(1:Tmax, function(t) range(preds[[t]]$state)))
+
+
+    pdf(sprintf("data-application/tests/init-field.pdf"))
+    fields::quilt.plot(locs[, 1], locs[, 2], as.numeric(XY$x0), zlim = zlim_x, nx = sqrt(N), ny = sqrt(N), main = "init")
+    dev.off()
+
+    for (t in 1:Tmax) {
+        pdf(sprintf("data-application/tests/test-field-%d.pdf", t), width=18, height=6)
+        oldpar = par(mfrow = c(1, 3))
+        fields::quilt.plot(locs[, 1], locs[, 2], as.numeric(XY$x[[t]]), zlim = zlim_x, nx = sqrt(N), ny = sqrt(N), main = sprintf("truth %d", t))
+        fields::quilt.plot(locs[, 1], locs[, 2], as.numeric(XY$y[[t]]), zlim = zlim_y, nx = sqrt(N), ny = sqrt(N), main = sprintf("data %d", t))
+        fields::quilt.plot(locs[, 1], locs[, 2], as.numeric(preds[[t]]$state), zlim = zlim_p, nx = sqrt(N), ny = sqrt(N), main = "reconstruction")
+        par(oldpar)
+        dev.off()
+    }
+    par(oldpar)
+}
+
+
+
+plotParamPaths = function(particles, indices, data.model, truth) {
+
+    Tmax = length(particles)
+    time = 1:Tmax
+    
+    ## parameters
+    pdf(sprintf("data-application/tests/%s-particles-over-time.pdf", data.model), width = 6, height = 4)
+    oldpar = par(mfrow = c(2, 3))
+
+    for (t in time) {
+        inds = indices[[t]]
+        particles[[t]] = particles[[t]][inds, ]
+    }
+
+    
+    for (par.name in colnames(particles[[1]])) {    
+
+        uq = sapply(particles, function(t) as.numeric(quantile(t[inds, par.name], 0.95)))
+        means = sapply(particles, function(t) as.numeric(mean(t[inds, par.name])))
+        lq = sapply(particles, function(t) as.numeric(quantile(t[inds, par.name], 0.05)))
+        
+        if (all(uq == lq)) {
+            cat(sprintf("%s was not sampled\n", par.name))
+            next()
+        }
+    
+        ylim = range(c(uq, lq))
+        ylim[2] = 1.1 * ylim[2]
+        plot(time, uq, type = "l", lty = 2, ylim = ylim, ylab = par.name, main = par.name)
+        lines(time, means, type = "l")
+        lines(time, lq, type = "l", lty = 2)
+        abline(h = truth[[par.name]], col = "red")
+    }
+    par(oldpar)
+    dev.off()
+}
+
+
+
+
+plot1dLogLikelihood = function(all_particles, logliks, truth) {
+
+    particles = all_particles[[length(all_particles)]]
+    all_params = colnames(particles)
+    
+    params = c()
+    for (par_name in all_params) {
+        unique_particles = unique(particles[, par_name])
+        if (length(unique_particles)>1) {
+            params = c(params, par_name)
+        }
+    }
+
+    oldpar = par(mfrow = c(1, length(params)))
+
+    for (param in params) {
+        this_param = particles[, param]
+        ord = order(this_param)
+        loglik = logliks[[length(logliks)]][ord]
+        plot(this_param[ord], loglik, type = "l", xlab = param, ylab = "loglikelihood",
+             main = sprintf("likelihood at time t=%d", length(logliks)))
+        abline(v = truth[[param]], col = "red")
+    }
+    par(oldpar)
+}
+
+
+plotMarginalDistribution = function(all_particles, all_indices, truth) {
+
+    inds = all_indices[[length(all_indices)]]
+    particles = all_particles[[length(all_particles)]]
+    all_params = colnames(particles)
+    
+    params = c()
+    for (par_name in all_params) {
+        unique_particles = unique(particles[, par_name])
+        if (length(unique_particles)>1) {
+            params = c(params, par_name)
+        }
+    }
+
+    oldpar = par(mfrow = c(1, length(params)))
+    for (param in params) {
+        hist(particles[inds, param], xlab = param, main = sprintf("Histogram of %s", param))
+        abline(v = truth[[param]], col = "red", lwd = 2, lty = 2)
+    }
+    par(oldpar)
+    
+}
--- data-application/test-filter.r
+++ data-application/test-filter.r
@@ -1,8 +1,9 @@
 ## imports ------------------------------------
 setwd("~/vecchiaFilter")
+source("data-application/settings.r")
 source("data-application/particle-filtering.r")
 source("aux-functions.r")
-source("data-application/settings.r")
+source("data-application/plot-results.r")
 library(doParallel)
 registerDoParallel(cores = NCORES)
 
@@ -24,59 +25,10 @@ mra = GPvecchia::vecchia_specify(locs, COND_SET_SIZE, conditioning = 'mra')
 predsMRA = filter(mra, XY$y, N_PARTS, lik.params, init_covparms, saveUQ = "L")
 
 
-## Plots ---------------------------------------- 
-# latent field + likelihood
-zlim_x = range(sapply(1:TMAX, function(t) range(XY$x[[t]])), XY$x0)
-zlim_y = range(sapply(1:TMAX, function(t) range(XY$y[[t]])))
-zlim_p = range(sapply(1:TMAX, function(t) range(predsMRA$preds[[t]]$state)))
+## Plots ----------------------------------------
+true_params = list(c = C, sig2 = SIG2, range = RANGE)
 
-
-pdf(sprintf("data-application/tests/init-field.pdf"))
-fields::quilt.plot(locs[, 1], locs[, 2], as.numeric(XY$x0), zlim = zlim_x, nx = sqrt(N), ny = sqrt(N), main = "init")
-dev.off()
-
-for (t in 1:TMAX) {
-    pdf(sprintf("data-application/tests/test-field-%d.pdf", t), width=18, height=6)
-    oldpar = par(mfrow = c(1, 3))
-    fields::quilt.plot(locs[, 1], locs[, 2], as.numeric(XY$x[[t]]), zlim = zlim_x, nx = sqrt(N), ny = sqrt(N), main = sprintf("truth %d", t))
-    fields::quilt.plot(locs[, 1], locs[, 2], as.numeric(XY$y[[t]]), zlim = zlim_y, nx = sqrt(N), ny = sqrt(N), main = sprintf("data %d", t))
-    fields::quilt.plot(locs[, 1], locs[, 2], as.numeric(predsMRA$preds[[t]]$state), zlim = zlim_p, nx = sqrt(N), ny = sqrt(N), main = "reconstruction")
-    par(oldpar)
-    dev.off()
-}
-par(oldpar)
-
-
-## parameters
-pdf(sprintf("data-application/tests/%s-particles-over-time.pdf", DATA_MODEL), width = 6, height = 4)
-oldpar = par(mfrow = c(2, 3))
-
-for (par.name in colnames(predsMRA$particles[[1]])) {    
-    time = 1:TMAX
-    
-    uq = sapply(predsMRA$particles, function(t) as.numeric(quantile(t[, par.name ], 0.9)))
-    means = sapply(predsMRA$particles, function(t) as.numeric(mean(t[, par.name ])))
-    lq = sapply(predsMRA$particles, function(t) as.numeric(quantile(t[, par.name ], 0.1)))
-
-    if (all(uq == lq)) {
-       cat(sprintf("%s was not sampled\n", par.name))
-       next()
-    }
-    
-    ylim = range(c(uq, lq))
-    ylim[2] = 1.1 * ylim[2]
-    plot(time, uq, type = "l", lty = 2, ylim = ylim, ylab = par.name, main = par.name)
-    lines(time, means, type = "l")
-    lines(time, lq, type = "l", lty = 2)    
-}
-par(oldpar)
-dev.off()
-
-
-## likelihood
-param = "sig2"
-truth = list(c = C, sig2 = SIG2, range = RANGE)
-params = predsMRA$particles[[TMAX]][, param]
-ord = order(params)
-plot(params[ord], predsMRA$logliks[[1]][ord], type = "l", xlab = param, ylab = "likelihood")
-abline(v = truth[[param]], col = "red")
+plotFields(XY, predsMRA$preds, locs)
+plotParamPaths(predsMRA$particles, predsMRA$resampled.indices, DATA_MODEL, true_params)
+#plot1dLogLikelihood(predsMRA$particles, predsMRA$logliks, true_params)
+plotMarginalDistribution(predsMRA$particles, predsMRA$resampled.indices, true_params)
